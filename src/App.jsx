9e6734f6a7ab31df248feffa41103ed51cb72446
GiampieroFC <giampieroferminicastillo@gmail.com>
2024-01-23 17:27:42 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import { GalleryInfo } from './components/GalleryInfo';
 import { ClientGallery } from "./pages/ClientGallery";
 import { RoutesApp } from "./routes/RoutesApp";
 
 function App() {
   return (
     <>
-      <RoutesApp />
+      <GalleryInfo />
+      {/* <RoutesApp /> */}
       {/* <ClientGallery /> */}
     </>
   );
