1a7ac2d678f7c198a1f880e3bc996154a103e9db
Maribel <marrodgar30@gmail.com>
2024-02-15 14:04:12 +0100

    "Cambios"

--- src/components/add-gallery/AddGallery.jsx
+++ src/components/add-gallery/AddGallery.jsx
@@ -18,24 +18,25 @@ const AddGallery = () => {
 
     const { title, client, minPics, totalPrice } = e.target;
 
-    if (
-      !title.value
-      || !client.value
-      || !minPics.value
-      || !totalPrice.value
-    ) {
+    if (!title.value || !client.value || !minPics.value || !totalPrice.value) {
       return;
     }
 
     setIsLoadin((prev) => true);
     const urlPhotoClaud = await urlPhotoC(photos);
 
+    const photosToBack = urlPhotoClaud.map(
+      (url) =>
+        url ??
+        "https://res.cloudinary.com/ddre5ln9t/image/upload/v1707667398/bmdngs4qqtk1cgiqq03x.png"
+    );
+
     const data = {
       title: title.value,
       client: client.value,
       minPics: minPics.value,
       totalPrice: totalPrice.value,
-      photos: urlPhotoClaud,
+      photos: photosToBack,
     };
 
     const gallery = new Gallery();
@@ -81,12 +82,14 @@ const AddGallery = () => {
           id="client"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mt-4"
         >
-          <option value=''>Clientes</option>
-          {clients.map((c) => (
-            <option key={c._id} value={c._id}>
-              {c.name} - {c.email}
-            </option>
-          )).reverse()}
+          <option value="">Clientes</option>
+          {clients
+            .map((c) => (
+              <option key={c._id} value={c._id}>
+                {c.name} - {c.email}
+              </option>
+            ))
+            .reverse()}
         </select>
       </div>
 
@@ -132,15 +135,19 @@ const AddGallery = () => {
         IMÁGENES
       </h5>
 
-      <MyDropzone setPhotos={setPhotos} currentPhotos={photos} isLoading={isLoading} />
+      <MyDropzone
+        setPhotos={setPhotos}
+        currentPhotos={photos}
+        isLoading={isLoading}
+      />
 
       {!!photos && (
         <ul className="max-h-28 overflow-auto">
           {photos.reverse().map((ph) => (
             <li className="m-1 p-2 flex gap-2 items-center" key={ph.path}>
               📸 - {ph.name}
-              {
-                isLoading || <svg
+              {isLoading || (
+                <svg
                   className="cursor-pointer text-[#7C7C7C] font-extrabold text-xl Tenor-Sans items-center w-6 h-6"
                   onClick={() => deletePhoto(ph.path)}
                   xmlns="http://www.w3.org/2000/svg"
@@ -155,7 +162,7 @@ const AddGallery = () => {
                     d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                   />
                 </svg>
-              }
+              )}
             </li>
           ))}
         </ul>
@@ -167,7 +174,6 @@ const AddGallery = () => {
         <button
           type="submit"
           className=" w-full text-white bg-[#D9D9D9] hover:bg-[#C7C7C7] focus:ring-4 focus:outline-none  font-medium rounded-lg text-base my-7 px-5 py-2.5 text-center "
-
         >
           CREAR
         </button>
